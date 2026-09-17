5c3a5c81480df0ebd32a3d12630cf050b07ab8f0
kindleschneider27-star <kindle.schneider27@gmail.com>
2026-09-16 19:21:00 -0700

    project created

---
+++ src/main/java/com/example/kindleschneiderinventroyapp/KindleSchneiderInventroyAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kindleschneiderinventroyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindleSchneiderInventroyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindleSchneiderInventroyAppApplication.class, args);
+    }
+
+}
